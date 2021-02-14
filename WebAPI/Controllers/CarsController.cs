1aa4107cedc6ef9433cfa3b809d50b2c7fc27003
armaganbice <armaganbice@hotmail.com>
2021-02-14 18:57:31 +0300

    14.02.2021

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,33 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+   
+    public class CarsController : Controller
+    {
+        [HttpGet]
+        public  List<Car> Get()
+        {
+            return new List<Car> {
+                new Car
+                {
+                    Id = 1,
+                    Description = "Renault"
+                },
+                new Car
+                {
+                    Id = 1,
+                    Description = "Renault"
+                }
+            };
+
+        }
+    }
+}
